8ab39509cb25978a11364e4cfec593126d8f7582
Jase Scott <144859802+JScott3234@users.noreply.github.com>
2024-07-09 15:58:34 -0500

    1.0. patch
    a stable release of threads and html file support, along with a test .html file. Completes the initial roadmap.

--- server.java
+++ server.java
@@ -1,29 +1,19 @@
 import java.io.*;
 import java.net.*;
-import java.util.*;
 
 public class server{
-    public static void main(String[] args) throws Exception {
-        try (ServerSocket server = new ServerSocket(8080)) {
-            System.out.println("Listening for connection on port 8080");
-            while(true){
-                try(Socket cliSock = server.accept()){
-                    //clientRequestView(cliSock);
-                    Date today = new Date();
-                    String hTextResp = "HTTP/1.1 200 OK\r\n\r\n" + today;
-                    cliSock.getOutputStream().write(hTextResp.getBytes("UTF-8"));
+    static final int port = 8080;
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket s = new ServerSocket(port)) {
+            System.out.println("Listening for connection on port " + port);
+                while(true){
+                    Socket cliSock = s.accept();
+                    Runnable service = new clicon(cliSock);
+                    Thread serv = new Thread(service);
+                    serv.start();
                 }
-            }
-        }
-    }
-
-    public static void clientRequestView(Socket clientSocket) throws IOException{
-        InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
-        BufferedReader reader = new BufferedReader(isr);
-        String line = reader.readLine();
-        while(!line.isEmpty()){
-            System.out.println(line); // prints out the browser request
-            line = reader.readLine();
+        } catch (Exception e){
+            e.printStackTrace();
         }
     }
 }
